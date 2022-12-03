a28ea556cda6e0d598a85cd7ea532cdb8ae23a73
Jonas Ishøj Bauch <jib090994@gmail.com>
2022-12-03 14:03:46 +0100

    updated wishlist tables

--- database/migrations/2022_12_03_125636_create_wishlists_table.php
+++ database/migrations/2022_12_03_125636_create_wishlists_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('wishlists', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->string('name');
             $table->timestamps();
         });
     }
